ef16ee28bca2d19cf33acd509ca0cf69bebe1b51
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-25 11:58:00 -0300

    componentizando o header

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,8 @@
 <script>
+  import Header from "./UI/Header.svelte";
 </script>
 
-<header>
-  <h1>MeetUs</h1>
-</header>
+<Header />
 
 <style>
-  header {
-    position: fixed;
-    width: 100%;
-    height: 4rem;
-    top: 0;
-    left: 0;
-    background: #cf0056;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 2px 6px rgba(0,0,0,0.26);
-  }
-
-  h1 {
-    color: white;
-    font-family: 'Roboto Slab', serif;
-    margin: 0;
-  }
 </style>
