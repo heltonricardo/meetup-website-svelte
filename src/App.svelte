1963558afabbc1c2c6049818ed41ab6848190dac
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-31 10:57:47 -0300

    adicionando valor padrao a propriedades para evitar warnings

--- src/App.svelte
+++ src/App.svelte
@@ -68,28 +68,24 @@
       id="title"
       label="Title"
       value={title}
-      type="text"
       on:input={(event) => (title = event.target.value)}
     />
     <TextInput
       id="subtitle"
       label="Subtitle"
       value={subtitle}
-      type="text"
       on:input={(event) => (subtitle = event.target.value)}
     />
     <TextInput
       id="address"
       label="address"
       value={address}
-      type="text"
       on:input={(event) => (address = event.target.value)}
     />
     <TextInput
       id="imageUrl"
       label="Image URL"
       value={imageUrl}
-      type="text"
       on:input={(event) => (imageUrl = event.target.value)}
     />
     <TextInput
