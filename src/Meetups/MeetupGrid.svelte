5b5a19db526e6629d491c05c9ec2c60cf1d0183c
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-11 09:54:07 -0300

    implementado filtro de favoritos

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,42 @@
 <script>
+  import MeetupFilter from "./MeetupFilter.svelte";
   import MeetupItem from "./MeetupItem.svelte";
 
   export let meetups;
+
+  let favsOnly = false;
+
+  $: filteredMeetups = favsOnly ? meetups.filter((m) => m.isFavorite) : meetups;
+
+  function setFilter(event) {
+    favsOnly = event.detail;
+  }
 </script>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
 </style>
 
+<section id="meetup-controls">
+  <MeetupFilter on:favsOnly={setFilter} />
+</section>
 <section id="meetups">
-  {#each meetups as meetup}
+  {#each filteredMeetups as meetup}
     <MeetupItem {meetup} on:toggleFavorite on:showdetails on:edit />
   {/each}
 </section>
