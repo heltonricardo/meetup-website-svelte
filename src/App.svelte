0c109960c35eb05091450bc16d545aad0d3b6123
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-16 10:53:36 -0300

    carregando e transformando os dados do servidor

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Button from "./UI/Button.svelte";
+  import { onMount } from "svelte";
   import Header from "./UI/Header.svelte";
   import EditMeetup from "./Meetups/EditMeetup.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
@@ -10,6 +10,24 @@
   let page = "overview";
   let editedId;
   let pageData = {};
+
+  onMount(() => {
+    fetch("https://meetup-meetus-default-rtdb.firebaseio.com/meetups.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Fetching meetups failed, please try again later!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const loadedMeetups = [];
+        for (const d in data) {
+          loadedMeetups.push({ id: d, ...data[d] });
+        }
+        meetups.setMeetups(loadedMeetups);
+      })
+      .catch((err) => console.log(err));
+  });
 
   function closeEditMode() {
     editMode = false;
@@ -42,19 +60,15 @@
 <main>
   {#if page === "overview"}
     {#if editMode}
-      <EditMeetup
-        id={editedId}
-        on:close={closeEditMode}
-      />
+      <EditMeetup id={editedId} on:close={closeEditMode} />
     {/if}
 
     <MeetupGrid
       meetups={$meetups}
       on:edit={startEdit}
       on:showdetails={showDetails}
-      on:add={() => editMode = true}
+      on:add={() => (editMode = true)}
     />
-
   {:else}
     <MeetupDetail id={pageData.id} on:close={closeDetails} />
   {/if}
--- src/Meetups/meetups-store.js
+++ src/Meetups/meetups-store.js
@@ -1,28 +1,6 @@
 import { writable } from "svelte/store";
 
-const meetups = writable([
-  {
-    id: 1,
-    title: "Coding Bootcamp",
-    subtitle: "Learn to code in 2 hours",
-    description:
-      "In this meetup, we will have some experts that teach you how to code!",
-    imageUrl: "https://www.enap.gov.br/media_files/images/imagem_bootcamp.jpg",
-    address: "27th Nerd Road, 32523 New York",
-    contactEmail: "code@test.com",
-    isFavorite: false,
-  },
-  {
-    id: 2,
-    title: "Swim Together",
-    subtitle: "Let's go for some swimming",
-    description: "Wee will simply swim some rounds!",
-    imageUrl: "https://pbs.twimg.com/media/Cx9mroxWIAIHHJP.jpg",
-    address: "27th Nerd Road, 32523 New York",
-    contactEmail: "swim@test.com",
-    isFavorite: false,
-  },
-]);
+const meetups = writable([]);
 
 const customMeetupsStore = {
   subscribe: meetups.subscribe,
@@ -46,5 +24,7 @@ const customMeetupsStore = {
       mtps.map((m) => (m.id !== id ? m : { ...m, isFavorite: !m.isFavorite }))
     );
   },
+
+  setMeetups: meetups.set,
 };
 export default customMeetupsStore;
